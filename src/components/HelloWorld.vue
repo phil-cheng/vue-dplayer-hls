6ed3e16604a08496ea6136c5b5c6ccba6730ff30
PhillCheng <565789229@qq.com>
2018-01-02 12:03:34 +0800

    main vue code

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+ <div class="hello">
+    <d-player ref="player" @play="play" :video="video" :contextmenu="contextmenu"></d-player>
+  </div>
+</template>
+
+<script>
+import VueDPlayer from './VueDPlayerHls';
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      video: {
+          url: 'https://logos-channel.scaleengine.net/logos-channel/live/biblescreen-ad-free/chunklist_w630020335.m3u8',
+          pic: 'http://static.smartisanos.cn/pr/img/video/video_03_cc87ce5bdb.jpg',
+          type: 'hls'
+        },
+        autoplay: false,
+        player: null,
+        contextmenu: [
+            {
+                text: 'GitHub',
+                link: 'https://github.com/MoePlayer/vue-dplayer'
+            }
+        ]
+    }
+  },
+  components: {
+    'd-player': VueDPlayer
+  },
+  methods: {
+    play() {
+        console.log('play callback')
+      }
+  },
+  mounted() {
+    this.player = this.$refs.player.dp;
+    // console.log(this.player);
+    var hls = new Hls();
+    hls.loadSource('https://logos-channel.scaleengine.net/logos-channel/live/biblescreen-ad-free/chunklist_w630020335.m3u8');
+    hls.attachMedia(this.player);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/VueDplayerHls.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="dplayer"></div>
+</template>
+
+<script>
+  require('../../node_modules/dplayer/dist/DPlayer.min.css');
+  import DPlayer from 'DPlayer'
+  export default {
+    props: {
+      autoplay: {
+        type: Boolean,
+        default: false
+      },
+      theme: {
+        type: String,
+        default: '#FADFA3'
+      },
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      lang: {
+        type: String,
+        default: 'zh'
+      },
+      screenshot: {
+        type: Boolean,
+        default: false
+      },
+      hotkey: {
+        type: Boolean,
+        default: true
+      },
+      preload: {
+        type: String,
+        default: 'auto'
+      },
+      contextmenu: {
+        type: Array
+      },
+      logo: {
+        type: String
+      },
+      video: {
+        type: Object,
+        required: true,
+        validator(value) {
+          return typeof value.url === 'string'
+        }
+      }
+    },
+    data() {
+      return {
+        dp: null
+      }
+    },
+    mounted() {
+      const player = this.dp = new DPlayer({
+        element: this.$el,
+        autoplay: this.autoplay,
+        theme: this.theme,
+        loop: this.loop,
+        lang: this.lang,
+        screenshot: this.screenshot,
+        hotkey: this.hotkey,
+        preload: this.preload,
+        contextmenu: this.contextmenu,
+        logo: this.logo,
+        video: {
+          url: this.video.url,
+          pic: this.video.pic,
+          type: this.video.type
+        }
+      })
+      player.on('play', () => {
+        this.$emit('play')
+      })
+      player.on('pause', () => {
+        this.$emit('pause')
+      })
+      player.on('canplay', () => {
+        this.$emit('canplay')
+      })
+      player.on('playing', () => {
+        this.$emit('playing')
+      })
+      player.on('ended', () => {
+        this.$emit('ended')
+      })
+      player.on('error', () => {
+        this.$emit('error')
+      })
+    }
+  }
+</script> 
